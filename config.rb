920c2d3ae62a501128935ce9cafc10cbf90ee8d2
Jonnie <jonnie@destroytoday.com>
2011-06-20 23:38:14 -0700

    Added content to DestroyTwitter page

--- config.rb
+++ config.rb
@@ -5,5 +5,6 @@ css_dir = "site/css"
 sass_dir = "src/_css"
 images_dir = "site/images"
 javascripts_dir = "site/js"
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
