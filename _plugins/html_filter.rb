689723b46511e779e6a6149c2fb444f4629b00b2
Jonnie <jonnie@destroytoday.com>
2011-07-21 23:41:15 -0700

    Added code post and code block styling

--- _plugins/html_filter.rb
+++ _plugins/html_filter.rb
@@ -5,13 +5,13 @@ module Jekyll
   
     def size_images(input)
       newinput = input
-=begin      
+=begin
       input.scan(/<img src='([^']+)'/) {|url|
         image = Magick::Image.read("#{@context.registers[:site].dest}/#{url}").first
       
         newinput = newinput.gsub(/<img src='#{url}'/, "<img src='#{url}' width='#{image.columns}' height='#{image.rows}'")
       }
-=end      
+=end
       newinput
     end
     
