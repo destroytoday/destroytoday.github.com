bcc1361d001a6c5939680f6fe0ef086e1adf9749
Jonnie <jonnie@destroytoday.com>
2011-08-05 00:11:05 -0700

    Finished post, rm_post, and image

---
+++ libs/admin.rb
@@ -0,0 +1,179 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'time'
+require 'rmagick'
+require 'fileutils'
+
+module Jekyll
+  class Admin
+    def initialize
+      load_config
+    end
+    
+    def load_config
+      config = YAML.load_file('_config.yml')
+      
+      @source = config['source']
+      @destination = config['destination']
+    end
+    
+    def post title, image_path, time=Time.now.to_s
+      post = Post.new(title, image_path, Time.parse(time))
+      
+      post.write "#{@source}/blog/_posts"
+      post.copy_image "#{@source}/assets/blog"
+      post.create_thumb "#{@source}/assets/blog"
+
+      puts "Created new post: #{post.filename}"
+    end
+    
+    def image post, title, image_path
+      image = Image.new(post, title, image_path)
+      
+      image.copy "#{@source}/assets/blog"
+      image.create_fitted "#{@source}/assets/blog"
+    end
+    
+    def rm_post slug
+      post = Dir.glob("#{@source}/blog/_posts/*-*-*-#{slug}.maruku")
+      assets = "#{@source}/assets/blog/#{slug}"
+      
+      FileUtils.rm_rf(post)
+      FileUtils.rm_rf(assets)
+      
+      puts "Removed #{post}"
+      puts "Removed #{assets}"
+    end
+  end
+  
+  class Post
+    def initialize title, image_path, time
+      @title = title
+      @image = Magick::ImageList.new(image_path)
+      @time = time
+    end
+    
+    def slug
+      @title.gsub(' ', '-').gsub(/[^a-z0-9\-_]+/i, '').downcase
+    end
+    
+    def filename
+      "#{shorttime}-#{slug}.maruku"
+    end
+    
+    def shorttime
+      @time.strftime('%Y-%m-%d')
+    end
+    
+    def longtime
+      @time.strftime('%Y-%m-%d %H:%M:%S')
+    end
+    
+    def img_regex
+      /\S+\/(\S+)\.(\w+)$/i
+    end
+    
+    def img_ext
+      @image.filename.gsub(img_regex, '\\2')
+    end
+    
+    def thumb_hires_filename
+      "#{slug}-thumb-hires.#{img_ext}"
+    end
+    
+    def thumb_filename
+      "#{slug}-thumb.#{img_ext}"
+    end
+    
+    def write dir
+      path = "#{dir}/#{filename}"
+      
+      File.open(path, 'w') do |file|
+        file.puts "---"
+        file.puts "layout: blog-post"
+        file.puts "title: #{@title}"
+        file.puts "shortname: #{@title}"
+        file.puts "date: #{longtime}"
+        file.puts "category: blog"
+        file.puts "tags: []"
+        file.puts "thumbnail: "
+        file.puts "  image: #{thumb_filename}"
+        file.puts "  colors: ['000000', 'FFFFFF']"
+        file.puts "---"
+      end
+    end
+    
+    def copy_image dir
+      dir = "#{dir}/#{slug}"
+      
+      FileUtils.mkdir(dir) unless File.exists? dir
+      
+      FileUtils.cp(@image.filename, "#{dir}/#{thumb_hires_filename}")
+    end
+    
+    def create_thumb dir
+      dir = "#{dir}/#{slug}"
+      
+      FileUtils.mkdir(dir) unless File.exists? dir
+      
+      resized_thumb = @image.resize_to_fill(200, 200)
+      sharpened_thumb = resized_thumb.unsharp_mask(1.1, 1.0, 0.4)
+      
+      sharpened_thumb.write("#{dir}/#{thumb_filename}")
+      
+      resized_thumb.destroy!
+      sharpened_thumb.destroy!
+    end
+  end
+  
+  class Image
+    def initialize post, title, image_path
+      @post = post
+      @title = title
+      @image = Magick::ImageList.new(image_path)
+    end
+    
+    def slug
+      @post + "-" + @title.gsub(' ', '-').gsub(/[^a-z0-9\-_]+/i, '').downcase
+    end
+    
+    def regex
+      /\S+\/(\S+)\.(\w+)$/i
+    end
+    
+    def filename
+      "#{slug}.#{ext}"
+    end
+    
+    def ext
+      @image.filename.gsub(regex, '\\2')
+    end
+    
+    def hires_filename
+      "#{slug}-hires.#{ext}"
+    end
+    
+    def copy dir
+      dir = "#{dir}/#{@post}"
+      
+      FileUtils.mkdir(dir) unless File.exists? dir
+      
+      FileUtils.cp(@image.filename, "#{dir}/#{hires_filename}")
+    end
+    
+    def create_fitted dir
+      dir = "#{dir}/#{@post}"
+      
+      FileUtils.mkdir(dir) unless File.exists? dir
+      
+      resized_image = @image.resize_to_fit(666)
+      sharpened_image = resized_image.unsharp_mask(1.1, 1.0, 0.4)
+      
+      sharpened_image.write("#{dir}/#{filename}")
+      
+      resized_image.destroy!
+      sharpened_image.destroy!
+    end
+  end
+end
