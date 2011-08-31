ecf64b12c95c63185552921343be93780d789fa9
Jonnie <jonnie@destroytoday.com>
2011-08-31 09:17:16 -0700

    Added disqus_id liquid param

--- _plugins/post.rb
+++ _plugins/post.rb
@@ -3,7 +3,9 @@ module Jekyll
     alias :super_to_liquid :to_liquid
     
     def to_liquid
-      super_to_liquid.deep_merge({"slug" => slug})
+      super_to_liquid.deep_merge({
+        "slug" => self.slug,
+        "disqus_id" => "#{self.dir}/#{self.data["disqus_id"] || self.slug}" })
     end
   end
 end
